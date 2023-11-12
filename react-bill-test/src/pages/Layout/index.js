3013993bb438254b915f926eb455f257eb2d596f
lushaqi <173382976@qq.com>
2023-11-12 15:14:49 +0800

    study: 主题色定制化

--- react-bill-test/src/pages/Layout/index.js
+++ react-bill-test/src/pages/Layout/index.js
@@ -1,9 +1,14 @@
+import { Button } from "antd-mobile";
 import { Outlet } from "react-router-dom";
 const Layout = () => {
   return (
     <div>
       Layout
       <Outlet></Outlet>
+      <Button color="primary">全局按钮</Button>
+      <div className="purple">
+        <Button color="primary">局部按钮</Button>
+      </div>
     </div>
   );
 };
